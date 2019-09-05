124878995b3734406def7f0d7723dfd03eac9e97
havuong <havuong138@gmail.com>
2019-09-06 04:38:59 +0700

    Accessing Elements and Interacting With Them

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -2,5 +2,8 @@
 // This triple comment line will tell VS Code for which package it needs autocomplete support for.
 
 it('should navigate to the TodoMVC App', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/');
+    cy.visit('http://todomvc-app-for-testing.surge.sh/')
+    cy.get('.new-todo').type('Clean room{enter}')
+    cy.get('.toggle').click()
+    cy.contains('Clear completed').click()
 });
